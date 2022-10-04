6bd2fb744362dafb032b6c5e505faa25bb52b1fe
Yakub Ali <mamuzaan@yahoo.com>
2022-10-04 22:54:57 +0000

    Connect post detail link to index.html and make code to urls.py

--- rsnblog/urls.py
+++ rsnblog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
