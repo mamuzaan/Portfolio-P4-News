42c9ce3e9715d471f506bea71aa193ca70b37b99
Yakub Ali <mamuzaan@yahoo.com>
2022-10-05 08:37:26 +0000

    Create a new file forms.py and import comment model and add context to views.py

--- rsnblog/views.py
+++ rsnblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
@@ -25,7 +26,8 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
-                "liked": liked
+                "liked": liked,
+                "comment_form": CommentForm()
             }
         )
         
