a427a8fb37b333e7a7ffff151d1410c6e8fec7c5
Yakub Ali <mamuzaan@yahoo.com>
2022-10-05 20:31:59 +0000

    Added id commentmsg and create script function for leave comments

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,9 @@ setTimeout(function(){
     let alert = new bootstrap.Alert(messages);
     alert.close();
 }, 2000);
+
+setTimeout(function(){
+    let messages = document.getElementById("commentmsg");
+    let alert = new bootstrap.Alert(messages);
+    alert.close();
+}, 3000);
