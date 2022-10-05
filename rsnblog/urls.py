d144ef801ae38c20e21be0e8cc2a66dc429b164e
Yakub Ali <mamuzaan@yahoo.com>
2022-10-05 23:33:30 +0000

    Added AddView path in urls.py

--- rsnblog/urls.py
+++ rsnblog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('add/', views.AddView.as_view(), name='add'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
