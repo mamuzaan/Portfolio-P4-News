f8ed78109cc570e6d695fa9372d4537f2ade5153
Yakub Ali <mamuzaan@yahoo.com>
2022-10-13 18:24:31 +0000

    Added sommernote_config to settings.py and add style iframe in style.css

--- anushilon2022/settings.py
+++ anushilon2022/settings.py
@@ -171,3 +171,12 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SUMMERNOTE_CONFIG = {
+    # You can put custom Summernote settings
+    'summernote': {
+        # Change editor size
+        'width': '100%',
+        'height': '500'
+    },
+}
