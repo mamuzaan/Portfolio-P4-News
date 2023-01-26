cc1d7f6a2895c6e706654e713a985ba0c83fd786
Yakub Ali <mamuzaan@yahoo.com>
2023-01-26 22:04:00 +0000

    Added if condition to database urls

--- anushilon2022/settings.py
+++ anushilon2022/settings.py
@@ -112,9 +112,21 @@ WSGI_APPLICATION = 'anushilon2022.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
